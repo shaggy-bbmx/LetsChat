14475956c1687ae13c8caf28ebffd7abc5121fea
sagar_patel <sagar.bcpatel@gmail.com>
2023-12-07 16:07:21 +0530

    original

--- frontend/src/Contexts/ChatContext.js
+++ frontend/src/Contexts/ChatContext.js
@@ -166,8 +166,8 @@ export const ChatContextProvider = ({ children, user }) => {
 
     //TO ESTABLISH SOCKET CONNECTION
     useEffect(() => {
-        // const newSocket = io('https://letschat-207o.onrender.com')
-        const newSocket = io('http://localhost:4000') 
+        const newSocket = io('https://letschat-207o.onrender.com')
+        // const newSocket = io('http://localhost:4000') 
         setSocket(newSocket)
 
 

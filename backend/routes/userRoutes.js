f941e8ea804a3690a66eb8ff36ca60345129df5f
shaggy-bbmx <54467552+shaggy-bbmx@users.noreply.github.com>
2024-04-25 12:53:58 +0530

    Update userRoutes.js
    Changed image CDN server

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const dotenv = require('dotenv')
+dotenv.config({ path: "../config/config.env" })
 const { registerUser, loginUser, findUser, getUsers } = require('../controller/userController')
 const isAuth = require('../middleware/isAuth')
 const Multer = require('multer')
 const { Storage } = require('@google-cloud/storage')
 const { v4: uuidv4 } = require('uuid')
 const userModel = require('../models/userModels')
+const { uploadFile, info } = require('@uploadcare/upload-client')
 
 
 
@@ -33,14 +36,27 @@ router.get('/', isAuth, getUsers)
 
 router.post('/uploadPic', multer.single('image'), async (req, res) => {
     try {
-        let name = uuidv4() + '.jpg'
-        const resp = await bucket.file(name).save(req.file.buffer)
+        // All This code is for uploading to Google Cloud Console which is no longer free-----------
+        // let name = uuidv4() + '.jpg'
+        // const resp = await bucket.file(name).save(req.file.buffer)
 
-        const [files] = await bucket.getFiles()
-        const targetFile = files.find((file) => file.name === name)
-        const url = `https://storage.googleapis.com/${targetFile.bucket.name}/${targetFile.id}`
+        // const [files] = await bucket.getFiles()
+        // const targetFile = files.find((file) => file.name === name)
+        // const url = `https://storage.googleapis.com/${targetFile.bucket.name}/${targetFile.id}`
+        // ------------------------------------------------------------------------------------------
 
-        const user = await userModel.findOneAndUpdate({ email:req.body.email }, { url }, { new: true })
+        const result = await uploadFile(req.file.buffer, {
+            publicKey: process.env.UPLOAD_CARE,
+            store: 'auto',
+            metadata: {
+                subsystem: 'js-client',
+                pet: 'cat'
+            }
+        })
+        
+        const url = 'https://ucarecdn.com/' + `${result.uuid}` + '/original'
+        console.log(url)
+        const user = await userModel.findOneAndUpdate({ email: req.body.email }, { url }, { new: true })
         res.status(200).json({ success: true })
 
     } catch (error) {
